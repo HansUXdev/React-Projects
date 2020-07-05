646c322ada17b8695673e4242875287e2ae18ac3
Hans <bmm79@nau.edu>
2020-07-05 12:33:06 -0700

    add buttons to storybook

--- hooks/src/stories/1-useState.stories.js
+++ hooks/src/stories/1-useState.stories.js
@@ -2,23 +2,28 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
 
-// import ToggleBtn from './useState/toggleBtn'
+import StarRating from '../useState/ProductRating/ProductRating'
+import ToggleBTN from '../useState/subscribeBTN/toggleBtn'
 
 export default {
     title: 'useState',
     component: Button,
 };
 
-export const Text = () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
+export const SubscribeBTN = () => (
+    <ToggleBTN />
 );
+// // import StarRating from './useState/ProductRating/ProductRating';
+export const ProductRating = () => {
+    return <StarRating totalStars={5} /> 
+}
 
-export const Emoji = () => (
-    <Button onClick={action('clicked')}>
-        <span role="img" aria-label="so cool">
-            😀 😎 👍 💯
-        </span>
-    </Button>
-);
+// export const Emoji = () => (
+//     <Button onClick={action('clicked')}>
+//         <span role="img" aria-label="so cool">
+//             😀 😎 👍 💯
+//         </span>
+//     </Button>
+// );
 
 
