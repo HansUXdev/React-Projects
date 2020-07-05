1e8ca1921804fedc01f0a6ba53510f130c10ba1f
Hans <bmm79@nau.edu>
2020-07-05 10:11:12 -0700

    change export

--- hooks/src/useModal/useModal.js
+++ hooks/src/useModal/useModal.js
@@ -6,10 +6,12 @@ import { useState } from 'react'
  * 2. Declare a function toggle that changes the value of isShowing to be the opposite of what it is currently
  * 3. Return the value of isShowing and the toggle function from the Hook, so the component has access to them.
  */
-export const useModal = () => {
+const useModal = () => {
   const [isShowing, setIsShowing] = useState(false);
   const toggle = () =>  setIsShowing(!isShowing);
   return {
     isShowing, toggle,
   }
 }
+
+export default useModal;
