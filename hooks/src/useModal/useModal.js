686312c3394bd83c164cc7cb32a7333825f72127
Hans <bmm79@nau.edu>
2020-07-05 09:54:12 -0700

    create custom hook called useModal

---
+++ hooks/src/useModal/useModal.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+/**
+ * 
+ * 1. Instantiate new isShowing and setIsShowing state values to store the current view state of the modal
+ * 2. Declare a function toggle that changes the value of isShowing to be the opposite of what it is currently
+ * 3. Return the value of isShowing and the toggle function from the Hook, so the component has access to them.
+ */
+export const useModal = () => {
+  const [isShowing, setIsShowing] = useState(false);
+  const toggle = () =>  setIsShowing(!isShowing);
+  return {
+    isShowing, toggle,
+  }
+}
